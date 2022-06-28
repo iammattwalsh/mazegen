31523cba258d8c108a21c262748353b76e96b88f
MattDWalsh <matthew.david.walsh@gmail.com>
2022-06-28 11:49:22 -0700

    refactored tile direction storage, added tile finding and tile moving

--- mazegen.py
+++ mazegen.py
@@ -3,29 +3,29 @@
 Notes - 
 
 direction list: 
-0 - up/north
-1 - right/east
-2 - down/south
-3 - left/west
+0 - North
+1 - East
+2 - South
+3 - West
 
 tile list:
 0 - blank (not generated)
-1 - up
-2 - right
-3 - down
-4 - left
-5 - up + down
-6 - right + left
-7 - up + right
-8 - right + down
-9 - down + left
-10 - left + up
-11 - up + right + down
-12 - right + down + left
-13 - down + left + up
-14 - left + up + right
+1 - N
+2 - E
+3 - S
+4 - W
+5 - N + S
+6 - E + W
+7 - N + E
+8 - E + S
+9 - S + W
+10 - W + N
+11 - N + E + S
+12 - E + S + W
+13 - S + W + N
+14 - W + N + E
 
-includes down:
+includes S:
 3
 5
 8
@@ -34,7 +34,7 @@ includes down:
 12
 13
 
-includes right:
+includes E:
 2
 6
 7
@@ -44,7 +44,7 @@ includes right:
 14
 
 (if 4-way):
-15 - up + right + down + left
+15 - N + E + S + W
 
 blank:
  __ __ __ __ __ __ __ __ __ __
@@ -72,49 +72,95 @@ def gen_blank(width,height):
         this_row = []
         # iterate through columns
         for x in range(width):
-            # empty cell for each column (represented by '0')
-            this_row.append(0)
+            # empty tile for each column as empty list
+            this_row.append([])
         # add to maze list
         maze_list.append(this_row)
     return maze_list
 
-def gen_maze(maze_list):
-    start = (randint(0, len(maze_list) - 1), randint(0, len(maze_list[0]) - 1))
-
+def gen_maze_parent(maze_list,width,height):
+    # generate (x,y) starting coordinate
+    start = (randint(0, width - 1), randint(0, height - 1))
+    return start
     ...
 
-def show_maze(maze_list):
-    # list of ints that represent missing walls down or left respectively
-    down_list = [3,5,8,9,11,12,13]
-    right_list = [2,6,7,8,11,12,14]
+def gen_maze_child(maze_list,width,height,position):
+    # add tiles that are N, E, S, & W to list
+    open_tiles = [
+        # N
+        (position[0], position[1] - 1),
+        # E
+        (position[0] + 1, position[1]),
+        # S
+        (position[0], position[1] + 1),
+        # W
+        (position[0] - 1, position[1]),
+    ]
+    # remove invalid tiles
+    for tile in open_tiles:
+        # remove tiles that are out of bounds on x axis
+        if tile[0] < 0 or tile [0] == width:
+            open_tiles.pop(open_tiles.index(tile))
+        # remove tiles that are out of bounds on y axis
+        elif tile[1] < 0 or tile [1] == height:
+            open_tiles.pop(open_tiles.index(tile))
+        # remove tiles that are already generated
+        elif maze_list[tile[1]][tile[0]] != []:
+            open_tiles.pop(open_tiles.index(tile))
+    # choose next move or back N
+    if len(open_tiles) == 0:
+        ...
+    else:
+        next_move = choice(open_tiles)
+        print(next_move)
+        if next_move[1] < position[1]:
+            maze_list[position[1]][position[0]].append('N')
+            maze_list[position[1] - 1][position[0]].append('S')
+            print('N')
+        elif next_move[0] > position[0]:
+            maze_list[position[1]][position[0]].append('E')
+            maze_list[position[1]][position[0] + 1].append('W')
+            print('E')
+        elif next_move[1] > position[1]:
+            maze_list[position[1]][position[0]].append('S')
+            maze_list[position[1] + 1][position[0]].append('N')
+            print('S')
+        elif next_move[0] < position[0]:
+            maze_list[position[1]][position[0]].append('W')
+            maze_list[position[1]][position[0] - 1].append('E')
+            print('W')
+
+    return maze_list
+
+def show_maze(maze_list,width,height):
     # empty strings for maze and first row
     maze_string = ''
     first_row = ''
     # add top border and add to maze_string
-    for x in range(len(maze_list[0])):
+    for x in range(width):
         first_row += ' __'
     maze_string += first_row + '\n'
     # iterate through rows
-    for y in range(len(maze_list)):
-        # add left wall to each row
+    for y in range(height):
+        # add W wall to each row
         this_row = '|'
         # iterate through columns
-        for x in range(len(maze_list[y])):
-            # assign current cell to variable for simpler reading
-            cell = maze_list[y][x]
-            # add spaces if the cell has no bottom wall
-            if cell in down_list:
+        for x in range(width):
+            # assign current tile to variable for simpler reading
+            tile = maze_list[y][x]
+            # add spaces if the tile has no bottom wall
+            if 'S' in tile:
                 this_row += '  '
             # add bottom wall otherwise
             else:
                 this_row += '__'
-            # add space if cell is missing both bottom and right walls
-            if cell in down_list and cell in right_list:
+            # add space if tile is missing both bottom and E walls
+            if 'S' in tile and 'E' in tile:
                 this_row += ' '
-            # add small bottom wall if cell is only missing right wall
-            elif cell in right_list:
+            # add small bottom wall if tile is only missing E wall
+            elif 'E' in tile:
                 this_row += '_'
-            # add pip if cell has right wall
+            # add pip if tile has E wall
             else:
                 this_row += '|'
         # add row and new line to maze_string
@@ -126,26 +172,14 @@ def show_maze(maze_list):
 width = 10
 height = 10
 
-# test vars
-# width = 4
-# height = 4
-# maze_list = [[8,3,0,0],[0,2,0,0],[0,2,0,0],[0,2,0,0],]
-
 maze_list = gen_blank(width,height)
-print(show_maze(maze_list))
+print(show_maze(maze_list,width,height))
+print('start')
+start = gen_maze_parent(maze_list,width,height)
+print(start)
+print('next tiles')
+# print(gen_maze_child(maze_list,width,height,start))
 
 
 
-# print("""
-#  __ __ __ __ __ __ __ __ __ __
-# |__|__|__|__|__|__|__|__|__|__|
-# |__|__|__|__|__|__|__|__|__|__|
-# |__|__|__|__|__|__|__|__|__|__|
-# |__|__|__|__|__|__|__|__|__|__|
-# |__|__|__|__|__|__|__|__|__|__|
-# |__|__|__|__|__|__|__|__|__|__|
-# |__|__|__|__|__|__|__|__|__|__|
-# |__|__|__|__|__|__|__|__|__|__|
-# |__|__|__|__|__|__|__|__|__|__|
-# |__|__|__|__|__|__|__|__|__|__|
-# """)
+print(show_maze(gen_maze_child(maze_list,width,height,start),width,height))
