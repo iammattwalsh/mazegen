c86138d23b8b7c5aa9e2e169125be2b45cc9e528
MattDWalsh <matthew.david.walsh@gmail.com>
2022-11-03 18:28:43 -0700

    git add .DO NOT MERGE AS ISgit add . began work on method to test if a tile's opening is blocked (for use when tiles are locked in an invalid position)

--- JavaScript/mazegen.js
+++ JavaScript/mazegen.js
@@ -69,6 +69,8 @@ class Maze {
                 thisTileBuilt.addEventListener('click', e => {
                     if (!thisTile.lock && !e.ctrlKey) {
                         thisTile.rotCurrent += 1
+                        // thisTile.rotDiff = thisTile.rotCurrent - thisTile.rotCorrect
+                        thisTile.rotUpdate()
                         thisTileBuilt.style.transform = `rotate(${thisTile.rotCurrent * 90}deg)`
                         this.checkMaze()
                     }
@@ -77,6 +79,8 @@ class Maze {
                     e.preventDefault()
                     if (!thisTile.lock && !e.ctrlKey) {
                         thisTile.rotCurrent -= 1
+                        // thisTile.rotDiff = thisTile.rotCurrent - thisTile.rotCorrect
+                        thisTile.rotUpdate()
                         thisTileBuilt.style.transform = `rotate(${thisTile.rotCurrent * 90}deg)`
                         this.checkMaze()
                     }
@@ -142,6 +146,48 @@ class Maze {
             console.log('incomplete')
         }
     }
+    test () {
+        console.log(this.tileArray[0][0])
+        console.log(this.tileArray[1][0])
+        // console.log(this.tileArray[0][0].binCurrent[1])
+        // console.log(this.tileArray[1][0].binCurrent[3])
+
+        
+
+        // if (this.tileArray[0][0].binCurrent[1] == 1 && this.tileArray[1][0].binCurrent[(1-2 < 0 ? 1-2+4 : 1-2)] != 1) {
+        //     console.log('boop')
+        // }
+
+        for (let row = 0; row < this.height; row ++) {
+            for (let col = 0; col < this.width; col++) {
+                for (let dig = 0; dig < 4; dig ++) {
+                    let adjDig = dig - 2 < 0 ? dig + 2 : dig - 2
+                    if (this.tileArray[row][col].binCurrent[dig] == 1 && this.tileArray[row + 1][col].binCurrent[adjDig] != 1) {
+                        ////
+                        console.log(`row: ${row} col: ${col}`)
+                        console.log(dig)
+                        console.log(adjDig)
+                        console.log(this.tileArray[row][col].binCurrent[dig])
+                        console.log(this.tileArray[row + 1][col].binCurrent[adjDig])
+                        ////
+                        console.log('blocked')
+                    } else if (this.tileArray[row][col].binCurrent[dig] == 1) {
+                        ////
+                        console.log(`row: ${row} col: ${col}`)
+                        console.log(dig)
+                        console.log(adjDig)
+                        console.log(this.tileArray[row][col].binCurrent[dig])
+                        console.log(this.tileArray[row + 1][col].binCurrent[adjDig])
+                        ////
+                        console.log('not blocked')
+                    }
+                }
+            }
+
+        }
+
+        //2Y5luZVZa4pdYOjr
+    }
 }
 
 class Tile {
@@ -150,26 +196,31 @@ class Tile {
         const tileValStraight = [5,10]
         const tileValCorner = [3,6,12,9]
         const tileValT = [7,14,13,11]
-        if (tileValEnd.includes(tileVal)) {
+        this.tileVal = tileVal
+        if (tileValEnd.includes(this.tileVal)) {
             this.type = 'end'
-            this.rotCorrect = tileValEnd.indexOf(tileVal)
+            this.rotCorrect = tileValEnd.indexOf(this.tileVal)
             this.paths = this.recipeEnd()
-        } else if (tileValStraight.includes(tileVal)) {
+        } else if (tileValStraight.includes(this.tileVal)) {
             this.type = 'straight'
-            this.rotCorrect = tileValStraight.indexOf(tileVal)
+            this.rotCorrect = tileValStraight.indexOf(this.tileVal)
             this.paths = this.recipeStraight()
-        } else if (tileValCorner.includes(tileVal)) {
+        } else if (tileValCorner.includes(this.tileVal)) {
             this.type = 'corner'
-            this.rotCorrect = tileValCorner.indexOf(tileVal)
+            this.rotCorrect = tileValCorner.indexOf(this.tileVal)
             this.paths = this.recipeCorner()
-        } else if (tileValT.includes(tileVal)) {
+        } else if (tileValT.includes(this.tileVal)) {
             this.type = 't'
-            this.rotCorrect = tileValT.indexOf(tileVal)
+            this.rotCorrect = tileValT.indexOf(this.tileVal)
             this.paths = this.recipeT()
         }
         this.rotCurrent = Math.floor(maze.rng() * 4)
         this.lock = false
-        this.element = null
+        this.element
+        this.binCorrect
+        this.rotDiff
+        this.binCurrent
+        this.rotUpdate()
     }
     build () {
         let newTile = this.recipeBasic()
@@ -177,6 +228,17 @@ class Tile {
             newTile.appendChild(path)
         })
         return newTile
+    }
+    rotUpdate () {
+        this.rotDiff = this.rotCurrent - this.rotCorrect
+        while (this.rotDiff > 3) {
+            this.rotDiff -= 4
+        }
+        while (this.rotDiff < 0) {
+            this.rotDiff += 4
+        }
+        this.binCorrect = this.tileVal.toString(2).padStart(4,'0')
+        this.binCurrent = this.binCorrect.slice(this.rotDiff) + this.binCorrect.slice(0,this.rotDiff)
     }
     recipeBasic () {
         let newTile = document.createElementNS('http://www.w3.org/2000/svg','svg')
@@ -250,13 +312,10 @@ class Tile {
 // y.displayMaze()
 // z.displayMaze()
 
-let testMaze = new Maze(5)
+let testMaze = new Maze(5,5,'2Y5luZVZa4pdYOjr')
 testMaze.displayMaze()
 console.log(testMaze.mazeSeed)
 // console.log(testMaze.mazeArray)
-
-
-// tNTPxUGb12vCRnez -- seed that previously contained 4-way intersection
 
 // need ability to highlight connected sets of tiles - i.e. hover over tile holding button (shift?) and all connected tiles are highlighted
 // need ability to highlight tiles locked in invalid positions - not incorrect positions, but invalid e.g. an opening touching a wall
