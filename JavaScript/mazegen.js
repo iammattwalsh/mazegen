29ca371d0d122d58b75c2706949584a01fe3894a
MattDWalsh <matthew.david.walsh@gmail.com>
2022-11-04 23:58:18 -0700

    fixed tile unlock bug, added Tile.indices to track location in Maze.tileArray, refactored Maze.checkTileBlock to use Tile.indices

--- JavaScript/mazegen.js
+++ JavaScript/mazegen.js
@@ -28,6 +28,15 @@ class Maze {
         this.genBlank()
         this.posStart = [Math.floor(this.rng() * this.width),Math.floor(this.rng() * this.height)]
         this.recursiveCarve(this.posStart)
+
+
+
+
+
+
+
+
+        // this.connectedTiles = []
     }
     genBlank () {
         for (let y = 0; y < this.height; y++) {
@@ -66,6 +75,7 @@ class Maze {
                 let thisTileBuilt = thisTile.build()
                 thisTile.element = thisTileBuilt
                 thisRow.push(thisTile)
+                thisTile.indices = [this.mazeArray.indexOf(row), thisRow.indexOf(thisTile)]
                 thisTileBuilt.addEventListener('click', e => {
                     if (!thisTile.lock && !e.ctrlKey) {
                         thisTile.rotCurrent += 1
@@ -101,7 +111,7 @@ class Maze {
                         if (thisTile.lock) {
                             thisTileBuilt.getElementsByTagName('rect')[0].classList.add('lock')
                         } else {
-                            thisTile.getElementsByTagName('rect')[0].classList.remove('lock')
+                            thisTileBuilt.getElementsByTagName('rect')[0].classList.remove('lock')
                         }
                         this.checkTileBlock()
                     }
@@ -146,39 +156,39 @@ class Maze {
         }
     }
     checkTileBlock () {
-        // loop through rows
-        for (let row = 0; row < this.height; row ++) {
-            // loop through columns in row
-            for (let col = 0; col < this.width; col++) {
-                // set current tile to variable
-                let thisTile = this.tileArray[row][col]
+        this.tileArray.forEach(row => {
+            row.forEach(tile => {
                 // variable for number of blocked directions
-                let blockCount = 0
+                let blockedCount = 0
                 // put adjacent tile indices in array
-                let adjTiles = [[row, col -1],[row + 1, col],[row, col + 1],[row - 1, col]]
-                // loop through sides of tile to find openings
+                let adjTilesIndices = [
+                    [tile.indices[0], tile.indices[1] - 1],
+                    [tile.indices[0] + 1, tile.indices[1]],
+                    [tile.indices[0], tile.indices[1] + 1],
+                    [tile.indices[0] - 1, tile.indices[1]],
+                ]
                 for (let dig = 0; dig < 4; dig ++) {
                     // set variable for index of matching direction on adjacent tile
                     let adjDig = dig - 2 < 0 ? dig + 2 : dig - 2
-                    // if the direction is open & the tile is locked & the adjacent tile is out of bounds, increment blockCount
-                    if (thisTile.binCurrent[dig] == 1 && thisTile.lock && (adjTiles[dig][0] < 0 || adjTiles[dig][0] >= this.height || adjTiles[dig][1] < 0 || adjTiles[dig][1] >= this.width)) {
-                        blockCount++
-                    // if the direction is open & the tile is locked & the adjacent tile does not have a matching opening & the adjacent tile is locked, increment blockCount
-                    } else if (thisTile.binCurrent[dig] == 1 && thisTile.lock && this.tileArray[adjTiles[dig][0]][adjTiles[dig][1]].binCurrent[adjDig] != 1 && this.tileArray[adjTiles[dig][0]][adjTiles[dig][1]].lock) {
-                        blockCount++
+                    // if the direction is open & the tile is locked & the adjacent tile is out of bounds, increment blockedCount
+                    if (tile.binCurrent[dig] == 1 && tile.lock && (adjTilesIndices[dig][0] < 0 || adjTilesIndices[dig][0] >= this.height || adjTilesIndices[dig][1] < 0 || adjTilesIndices[dig][1] >= this.width)) {
+                        blockedCount++
+                    // if the direction is open & the tile is locked & the adjacent tile does not have a matching opening & the adjacent tile is locked, increment blockedCount
+                    } else if (tile.binCurrent[dig] == 1 && tile.lock && this.tileArray[adjTilesIndices[dig][0]][adjTilesIndices[dig][1]].binCurrent[adjDig] != 1 && this.tileArray[adjTilesIndices[dig][0]][adjTilesIndices[dig][1]].lock) {
+                        blockedCount++
                     }
-                    // set tile to blocked and add blocked class if blockCount indicates at least 1 block
-                    if (blockCount > 0) {
-                        thisTile.blocked = true
-                        thisTile.element.getElementsByTagName('rect')[0].classList.add('blocked')
-                    // set tile to unblocked and remove blocked class if blockCount indicates no blocks
+                    // set tile to blocked and add blocked class if blockedCount indicates at least 1 block
+                    if (blockedCount > 0) {
+                        tile.blocked = true
+                        tile.element.getElementsByTagName('rect')[0].classList.add('blocked')
+                    // set tile to unblocked and remove blocked class if blockedCount indicates no blocks
                     } else {
-                        thisTile.blocked = false
-                        thisTile.element.getElementsByTagName('rect')[0].classList.remove('blocked')
+                        tile.blocked = false
+                        tile.element.getElementsByTagName('rect')[0].classList.remove('blocked')
                     }
                 }
-            }
-        }
+            })
+        })
     }
     findConnected () {
         // highlight connected sets of tiles - i.e. hover over tile holding button (shift?) and all connected tiles are highlighted
@@ -191,7 +201,7 @@ class Maze {
 
     }
     findLoops () {
-        // highlight connected loops - i.e. four corner tiles connected to each other to form a square loop
+        // highlight connected loops - e.g. four corner tiles connected to each other to form a square loop
 
     }
 }
@@ -224,13 +234,14 @@ class Tile {
         this.lock = false
         this.blocked = false
         this.element
+        this.indices
         this.binCorrect
         this.rotDiff
         this.binCurrent
         this.rotUpdate()
     }
     build () {
-        let newTile = this.recipeBasic()
+        let newTile = this.recipeBase()
         this.paths.forEach(path => {
             newTile.appendChild(path)
         })
@@ -247,7 +258,7 @@ class Tile {
         this.binCorrect = this.tileVal.toString(2).padStart(4,'0')
         this.binCurrent = this.binCorrect.slice(this.rotDiff) + this.binCorrect.slice(0,this.rotDiff)
     }
-    recipeBasic () {
+    recipeBase () {
         let newTile = document.createElementNS('http://www.w3.org/2000/svg','svg')
         const tileBG = document.createElementNS('http://www.w3.org/2000/svg','rect')
         tileBG.setAttribute('width','40px')
@@ -320,6 +331,7 @@ class Tile {
 // z.displayMaze()
 
 let testMaze = new Maze(5,5,'2Y5luZVZa4pdYOjr')
+// let testMaze = new Maze(25)
 testMaze.displayMaze()
 console.log(testMaze.mazeSeed)
 // console.log(testMaze.mazeArray)
